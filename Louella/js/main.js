c41b699a6ff2282acec75d63447f7737aa29c41b
Ch0ccy-m1lk <louellahanen@outlook.com>
2025-06-23 10:16:02 +0200

    teehee

---
+++ Louella/js/main.js
@@ -0,0 +1,23 @@
+function AddCard(container, title, price, details) {
+    let article = document.createElement("article");
+    article.className = "card";
+    article.innerHTML = `
+        <h1>${title}</h1>
+        <p>price : $${price},-</p>
+        <p class="details">${details}</p>
+    `;
+    container.appendChild(article);
+}
+
+async function GetData() {
+    const response = await fetch("js/index.json");
+    const data = await response.json();
+    
+    const squares = document.querySelectorAll(".square");
+
+    for (let i = 0; i < data.cards.length && i < squares.length; i++) {
+        AddCard(squares[i], data.cards[i].title, data.cards[i].price, data.cards[i].details);
+    }
+}
+
+GetData();
